0197e7b77f5bbad5041c9ba6c0b2666a389de233
vHast <fedvhast@gmail.com>
2022-03-24 13:30:33 +0200

    Hex script

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ btn.addEventListener('click' , function(){
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 });
+
 // Randomizer, will multiply with the length of colors
 function getRandomNumber(){
   return Math.floor(Math.random() * colors.length)
--- hex.js
+++ hex.js
@@ -1,1 +1,19 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+// Hex (#) consist on numbers and letters
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+  let hexColor = '#';
+  // 6 stands for the digits of a Hex (#) color
+  for (let i = 0; i <6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+})
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
