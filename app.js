1d6407643691ed5a1565e4dd9461f09c1aae59a1
vHast <fedvhast@gmail.com>
2022-03-24 13:18:36 +0200

    Added randomizer

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const color = document.querySelector('.color');
 
 btn.addEventListener('click' , function(){
   // Get random number between 0 - 3 for colors[0-3]
-  const randomNumber = 2;
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber)
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
-})
+});
+// Randomizer, will multiply with the length of colors
+function getRandomNumber(){
+  return Math.floor(Math.random() * colors.length)
+}
